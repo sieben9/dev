122c6a8fa6479b5d39686d03f7375c6e5a7e9185
sieben9 <107976939+sieben9@users.noreply.github.com>
2023-10-17 10:03:47 +0200

    First push

---
+++ test.py
@@ -0,0 +1,13 @@
+print('Hallo')
+
+digit1 = 5
+digit2 = 12
+digit3 = 55
+
+sum = digit1 + digit2 + digit3
+
+print(sum)
+
+digitinput = int(input('Nenne mir eine Zahl: '))
+
+print('Danke. Die Zahl heißt:', digitinput)
